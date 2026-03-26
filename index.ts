b5546319a9a9ae2763619f95f9987b2396af86f8
Steve <contact@stevedylan.dev>
2026-03-26 19:48:00 -0400

    chore: re-arranged assets

--- index.ts
+++ index.ts
@@ -8,6 +8,14 @@ Bun.serve({
       const file = Bun.file(`./public${url.pathname}`);
       return new Response(file);
     },
+    "/*": async (req) => {
+      const url = new URL(req.url);
+      const file = Bun.file(`./public${url.pathname}`);
+      if (await file.exists()) {
+        return new Response(file);
+      }
+      return new Response("Not found", { status: 404 });
+    },
   },
   development: {
     hmr: true,
